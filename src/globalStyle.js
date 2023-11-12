2ee6177c459f89f648d61fa3562213f73578ab50
Cindy <xd753159@gmail.com>
2023-11-12 15:49:24 +0800

    feat: 新增 breakpoint 及全域 css 變數

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,6 +1,34 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+	:root {
+		--color-primary-gradient: linear-gradient(90deg, #E5793B 1.54%, #FF4185 97.86%);
+		--color-primary-theme-1: #DA7D4A;
+		--color-text-primary-50: #F8FAFC;
+		--color-text-primary-100: #F1F5F9;
+		--color-text-primary-200: #E2E8F0;
+		--color-text-primary-300: #CBD5E1;
+		--color-text-primary-400: #94A3B8;
+		--color-text-primary-500: #64748B;
+		--color-text-primary-600: #475569;
+		--color-text-primary-700: #334155;
+		--color-text-primary-800: #1E293B;
+		--color-text-primary-900: #0F172A;
+		--color-text-secondary: ##94A3B8;
+		--color-bg-theme-1: #FDFCFB;
+		--color-bg-theme-2: ##F7ECE1;
+	}
+
+	* {
+		font-family: Inter;
+		font-style: normal;
+	}
+
+	@font-face {
+		font-family: 'Mantou Sans';
+		src: url('/src/assets/font/MantouSans-Regular.ttf');
+	}
+
  /* http://meyerweb.com/eric/tools/css/reset/ 
 		v2.0 | 20110126
 		License: none (public domain)
@@ -23,7 +51,6 @@ const GlobalStyle = createGlobalStyle`
 		padding: 0;
 		border: 0;
 		font-size: 100%;
-		font: inherit;
 		vertical-align: baseline;
 	}
 	/* HTML5 display-role reset for older browsers */
@@ -51,4 +78,87 @@ const GlobalStyle = createGlobalStyle`
 	}
 `;
 
-export default GlobalStyle;
+const font = ({ color, size }) => `
+	background: ${color};
+	-webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+	font-size: ${size};
+`;
+
+const H1HeadingTitle = () => `
+  font-size: 40px;
+  line-height: 60px;
+  letter-spacing: -0.8px;
+	font-weight: 700;
+`;
+
+const H2HeadingTitle = () => `
+  font-size: 32px;
+  line-height: 48px;
+	font-weight: 700;
+	font-weight: 700;
+`;
+
+const H3HeadingTitle = () => `
+  font-size: 28px;
+  line-height: 42px;
+	font-weight: 700;
+`;
+
+const H4HeadingTitle = () => `
+  font-size: 24px;
+  line-height: 36px;
+	font-weight: 700;
+`;
+
+const H5HeadingTitle = () => `
+  font-size: 20px;
+  line-height: 30px;
+	font-weight: 700;
+`;
+
+const H6HeadingTitle = () => `
+  font-size: 16px;
+  line-height: 24px;
+	font-weight: 700;
+`;
+
+const BaseText = () => `
+	font-size: 18px;
+	font-weight: 400;
+	line-height: 27px;
+`;
+
+const NormalText = () => `
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 24px;
+`;
+
+const SmallText = () => `
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 21px;
+`;
+
+const BigText = () => `
+	font-family: Mantou Sans;
+	font-size: 52px;
+	font-weight: 400;
+	line-height: normal;
+`;
+
+export {
+  GlobalStyle,
+  font,
+  H1HeadingTitle,
+  H2HeadingTitle,
+  H3HeadingTitle,
+  H4HeadingTitle,
+  H5HeadingTitle,
+  H6HeadingTitle,
+  BaseText,
+  NormalText,
+  SmallText,
+  BigText,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./globalStyle.js";
+import { GlobalStyle } from "./globalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
